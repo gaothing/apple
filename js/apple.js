776c1cf109bd83ef579b6d24a4c9f106d8b7c56d
gaothing <744989745@qq.com>
2016-12-09 18:08:35 +0800

    cndsac

---
+++ js/apple.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$(".top .mune").click(function(){
+	$(".sl").removeClass("sl-act2").addClass("sl-act")
+	})
+	$(".sltop .close").click(function(){
+		
+		$(".sl").removeClass("sl-act").addClass("sl-act2")
+		
+		
+	})
+})
